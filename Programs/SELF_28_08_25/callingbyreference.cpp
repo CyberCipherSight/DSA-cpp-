3afc5999f54c683ad0bb6bde216e6e22e7162800
CyberCipherSight <150309717+CyberCipherSight@users.noreply.github.com>
2025-08-28 23:59:01 +0530

    zz

--- Programs/SELF_28_08_25/callingbyreference.cpp
+++ Programs/SELF_28_08_25/callingbyreference.cpp
@@ -1,32 +1,41 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
 
-
 class Solution {
-  public:
+public:
     int findElementAtIndex(int key, vector<int> &arr) {
-        // code here
-        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == key) {
+                return i; 
+            }
+        }
+        return -1; 
     }
 };
 
-
-int main(){
-
+int main() {
     int key;
-    cout<<"Enter key to find in array : ";
-    cin>>key;
-     int n;
+    cout << "Enter key to find in array : ";
+    cin >> key;
+
+    int n;
     cout << "Enter size of vector: ";
     cin >> n;
 
-    vector<int> v(n);  // create vector of size n
+    vector<int> v(n);
     cout << "Enter " << n << " elements: ";
     for (int i = 0; i < n; i++) {
-        cin >> v[i];   // user input
+        cin >> v[i];
     }
+
+    Solution sol1;
+    int result = sol1.findElementAtIndex(key, v);
+
+    if (result != -1)
+        cout << "Key found at index: " << result << endl;
+    else
+        cout << "Key not found!" << endl;
+
     return 0;
-    
 }
